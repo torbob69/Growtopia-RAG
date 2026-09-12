2576925bd5ef534f672d16321a63b1bd771ff3c6
torbob <nicholaswilson7803@gmail.com>
2026-09-12 20:20:48 +0700

    seeding function

--- fast-api-backend/vector-service/service.py
+++ fast-api-backend/vector-service/service.py
@@ -1,8 +1,17 @@
 import pickle
 from sqlalchemy.ext.asyncio import AsyncSession
 
-def seeding(db : AsyncSession):
-    vectors = pickle.load('C:/Growtopia-RAG/pipeline/vectors.pkl')
-    docs_splitted = pickle.load('C:/Growtopia-RAG/pipeline/docs_splitted.pkl')
-    
-    
+from models.models import Chunk
+
+async def seeding(db: AsyncSession):
+    with open('C:/Growtopia-RAG/pipeline/docs_splitted.pkl', 'rb') as f:
+        docs_splitted = pickle.load(f)
+    with open('C:/Growtopia-RAG/pipeline/vectors.pkl', 'rb') as f:
+        vectors = pickle.load(f)
+
+    chunks = [
+        Chunk(content=doc.page_content, source=doc.metadata['source'], embedding=vector)
+        for doc, vector in zip(docs_splitted, vectors)
+    ]
+    db.add_all(chunks)
+    await db.commit()
